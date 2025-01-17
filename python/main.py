7e0f2bdc8f0affefc793b31b69885cf2d99013e9
Fuyuan.Jing <Fuyuan.Jing@amd.com>
2025-01-17 15:04:59 -0500

    initial commit

--- python/main.py
+++ python/main.py
@@ -24,7 +24,7 @@ def benchmark(model_name, input_length, output_length, batch_size, device):
     start_time = time.time()
     with torch.no_grad():
         for _ in range(10):  # Run multiple iterations to get a stable throughput measurement
-            outputs = model.generate(**inputs, max_length=output_length, do_sample=False)
+            outputs = model.generate(**inputs, max_new_tokens=output_length, do_sample=True)
     throughput = (batch_size * 10) / (time.time() - start_time)
 
     print(f"Time to first token: {time_to_first_token:.4f} seconds")
@@ -37,6 +37,10 @@ if __name__ == "__main__":
     parser.add_argument("--output_length", type=int, default=20, help="Length of the output sequence")
     parser.add_argument("--batch_size", type=int, default=1, help="Batch size")
     parser.add_argument("--device", type=str, default="cpu", help="Device to run the model on (cpu or cuda)")
-
+    # todo: add do_sample argument
+    # todo: add temperature argument
+    # todo: add top_k argument
+    # todo: add top_p argument
+    # todo: add number of beams argument
     args = parser.parse_args()
     benchmark(args.model_name, args.input_length, args.output_length, args.batch_size, args.device)
